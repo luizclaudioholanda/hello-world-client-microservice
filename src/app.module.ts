1fd65e0461429313025ab16fdd69592100fed0fd
Luiz Claudio <luiz.holanda06@gmail.com>
2023-11-13 15:10:47 -0300

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ClientProxyFactory, Transport } from '@nestjs/microservices';
+import { AppController } from './app.controller';
+
+@Module({
+  imports: [ConfigModule.forRoot()],
+  controllers: [AppController],
+  providers: [
+    {
+      provide: 'HELLO_SERVICE',
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) =>
+        ClientProxyFactory.create({
+          transport: Transport.TCP,
+          options: {
+            host: configService.get<string>('HELLO_SERVICE_HOST'),
+            port: parseInt(configService.get<string>('HELLO_SERVICE_PORT')),
+          },
+        }),
+    },
+  ],
+})
+export class AppModule {}
